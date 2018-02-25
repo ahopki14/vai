bbaf13e5ac1d102d63e5d17c49372ab4c5d2e409
Alex Hopkins <achopkins@gmail.com>
2018-02-25 13:23:54 -0500

    Added R function to call lookup, and added code to call it for all variants

--- vai.R
+++ vai.R
@@ -18,8 +18,42 @@ sample_data <- DataFrame(GT=geno(v_singles)$GT,
 colnames(sample_data) <- c('GT','AO','RO') #not sure why this doesn't default
 
 #sanity check that the two DataFrames are in the same order
-stopifnot(all(rownames(sample_data)==rownames(i)))
+stopifnot(all(rownames(sample_data)==rownames(info_data)))
 
 #put them together
 data <- cbind(info_data, sample_data)
+
+
+#a function to call the shell script which does the ExAC lookup
+exac <- function(var){
+	out <- system(paste('sh exac_lookup.sh -cfp',var), intern=T)
+	#clean up the output (NA instead of empty or 'null')
+	out[out==''] <- NA
+	out[out=='null'] <- NA
+	#return
+	out
+}
+
+#Format the variant names for ExAC
+nms <- rownames(data)
+nms <- gsub('chr','',nms)
+nms <- gsub(':|_|\\/','-',nms)
+data$nms <- nms
+
+# Get the ExAC info for all 
+start <- proc.time()
+out <- mapply(data$nms, FUN=exac)
+#check the time to query
+t <- round((proc.time()-start)[['elapsed']]/60,2)
+cat(dim(out)[2],'queries in',t,'minutes\n')
+
+out <- as.data.frame(t(out))
+names(out) <- c('Consequence', 'Pop_Allele_Freq','PolyPhen')
+out$PolyPhen <- as.numeric(out$PolyPhen)
+out$Pop_Allele_Freq <- as.numeric(out$Pop_Allele_Freq)
+
+#sanity check the order
+stopifnot(all(rownames(out)==data$nms))
+
+data <- cbind(data,out)
 
