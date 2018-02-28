e4af47c2a341fcb6922971ac42eef107f5b44dda
Alex Hopkins <achopkins@gmail.com>
2018-02-28 13:08:27 -0500

    Modified simlify_vcf to identify records decomposed from variants with more than 2 alleles

--- R/simplify_vcf.R
+++ R/simplify_vcf.R
@@ -20,7 +20,7 @@ simplify_vcf <- function(vcf){
 	#identify rows which require fixes and fix them
 	#  the variant names, reference and alternate alleles and 
 	#  position are all fixed 
-	w <- grep('2', geno(vcf)$GT)
+	w <- grep('[2-9]', geno(vcf)$GT)
 	fixed_records  <- sapply(vcf[w], simplify_variant)
 
 	#sanity check that all records were checked
