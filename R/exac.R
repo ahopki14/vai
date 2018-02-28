cf1053a2e9227fbd7391f3a0ff579fc1a42d4f2e
Alex Hopkins <achopkins@gmail.com>
2018-02-28 12:48:51 -0500

    Modified exac function to be able to call the packaged version of the exac_lookup shellscript

--- R/exac.R
+++ R/exac.R
@@ -11,7 +11,8 @@
 
 #a function to call the shell script which does the ExAC lookup
 exac <- function(var){
-	out <- system(paste('sh exac_lookup.sh -cfps',var), intern=T)
+	path <- system.file('exec/exac_lookup.sh',package='vai')
+	out <- system(paste('sh',path,'-cfps',var), intern=T)
 	#clean up the output (NA instead of empty or 'null')
 	out[out==''] <- NA
 	out[out=='null'] <- NA
