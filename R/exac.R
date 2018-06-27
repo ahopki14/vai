f1eccbcde249b6aec4c09a790943347c8e50ce08
Alex Hopkins <achopkins@gmail.com>
2018-06-27 15:08:28 -0400

    removed part of function

--- R/exac.R
+++ R/exac.R
@@ -12,12 +12,6 @@
 #a function to call the shell script which does the ExAC lookup
 exac <- function(var){
 	#locate the lookup script in the package and call it
-	path <- system.file('exec','exac_lookup.sh',package='vai')
-	out <- system(paste('sh',path,'-cfps',var), intern=T)
-	#clean up the output (NA instead of empty or 'null')
-	out[out==''] <- NA
-	out[out=='null'] <- NA
-	#check that something was created
 	if(!length(out)>0){warning('Nothing generated from the query,
 				  make sure that curl is installed and 
 				  the exac_lookup.sh script is executable'
